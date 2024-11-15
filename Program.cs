b29bb13a53301ea6a4310b073f3b57a2e60eb662
carobte <caro.bustamante.escobar@gmail.com>
2024-11-15 06:35:15 -0500

    feat: initial readme and welcome page

--- Program.cs
+++ Program.cs
@@ -20,6 +20,30 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        var htmlContent = @"
+        <html>
+            <head>
+                <title>Clinic API</title>
+            </head>
+            <body style='font-family: Arial, sans-serif; background-color: #f4f4f4; text-align: center; height: 90%; display: flex; flex-direction: column; justify-content: center; align-items: center;'>
+                <h1 style='color: #333; font-size: 36px;'>Welcome to Clinic API</h1>
+                <a href='/swagger' style='color: #007bff; text-decoration: none;'> Click here to Swagger documentation</a>
+            </body>
+        </html>";
+
+        context.Response.ContentType = "text/html";
+        await context.Response.WriteAsync(htmlContent);
+    }
+    else
+    {
+        await next();
+    }
+});
+
 app.MapControllers();
 
 app.Run();
